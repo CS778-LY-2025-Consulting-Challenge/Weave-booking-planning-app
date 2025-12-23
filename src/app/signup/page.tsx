fb92b92028ba3f55fe34b633d3ee7a079edad7eb
Umesh <upnayak2512@gmail.com>
2025-12-23 19:21:28 +1300

    feature: Added animated login page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { Component as AnimatedSignupPage } from '@/components/animated-characters-signup-page';
+
+export default function Signup() {
+  return <AnimatedSignupPage />;
+}
